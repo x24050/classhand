f0a6c718996f5ca57ee0b86f4302458d5b75fad1
x24050 <x24050xx@aitech.ac.jp>
2025-10-20 22:02:05 +0900

    Update raise-hand.js

--- api/raise-hand.js
+++ api/raise-hand.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+/*export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method not allowed");
   }
@@ -59,4 +59,4 @@ export default async function handler(req, res) {
     console.error("Exception:", err);
     return res.status(500).send("Server error");
   }
-}
+}*/
